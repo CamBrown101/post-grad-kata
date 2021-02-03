2127d3830ffe9395d3e3f27cc864b62ce32d437b
cambrown101 <cameron-brown@live.com>
2021-02-02 21:32:13 -0800

    Complete

---
+++ TwoSum.js
@@ -0,0 +1,11 @@
+const twoSum = (numbers, target) => {
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+console.log(twoSum([1, 2, 3, 4, 5], 9));
