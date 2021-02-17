294be8869265d17e8b127aa85fbf89c16cb11037
cambrown101 <cameron-brown@live.com>
2021-02-17 14:25:32 -0800

    Complete

--- SortByLastChar.js
+++ SortByLastChar.js
@@ -1,5 +1,20 @@
 function last(x) {
   arrayOfWords = x.split(' ');
+  arrayOfletters = [];
+  arrayOfWords.forEach((word, i) => {
+    arrayOfletters.push(word[word.length - 1]);
+  });
+  sorted = arrayOfletters.sort();
+
+  const ans = [];
+  for (letter of sorted) {
+    for (word of arrayOfWords) {
+      if (letter === word[word.length - 1]) {
+        ans.push(word);
+      }
+    }
+  }
+  return ans;
 }
 
 console.log(last('man i need a taxi up to ubud'));
