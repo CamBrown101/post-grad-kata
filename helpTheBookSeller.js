45356deb634dadf0a86273196217329c4a867612
cambrown101 <cameron-brown@live.com>
2021-03-08 19:47:53 -0800

    Add 0s in order of listOfCat

--- helpTheBookSeller.js
+++ helpTheBookSeller.js
@@ -1,31 +1,23 @@
 function stockList(listOfArt, listOfCat) {
+  if (listOfArt.reduce((a, b) => a + b, 0) === 0) {
+    return '';
+  }
+
   let ans = {};
+  for (let letter of listOfCat) {
+    ans[letter] = 0;
+    console.log(ans);
+  }
   for (codes of listOfArt) {
     let firstLetter = codes[0];
     if (listOfCat.includes(firstLetter)) {
-      if (!ans.hasOwnProperty(firstLetter)) {
-        ans[firstLetter] = parseInt(codes.split(' ')[1]);
-      } else {
-        ans[firstLetter] += parseInt(codes.split(' ')[1]);
-      }
+      ans[firstLetter] += parseInt(codes.split(' ')[1]);
     }
   }
-
-  for (let cat of listOfCat) {
-    if (!ans.hasOwnProperty(cat)) {
-      ans[cat] = 0;
-    }
-  }
-  const orderedAns = Object.keys(ans)
-    .sort()
-    .reduce((obj, key) => {
-      obj[key] = ans[key];
-      return obj;
-    }, {});
 
   //Format answer
   let formattedAns = '';
-  for (let key in orderedAns) {
+  for (let key in ans) {
     formattedAns += `(${key} : ${ans[key]}) - `;
   }
   return formattedAns.slice(0, -3);
