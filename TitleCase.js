ee062e7b98df73f5253e6847390cd0e13a2e6e62
cambrown101 <cameron-brown@live.com>
2021-03-08 18:24:03 -0800

    Complete

--- TitleCase.js
+++ TitleCase.js
@@ -1,16 +1,18 @@
 function titleCase(title, minorWords) {
-  const wordsArray = title.split(' ');
+  const wordsArray = title.toLowerCase().split(' ');
   let firstLetters = [];
   let formattedWords = '';
   let minorWordsArray = [];
   if (minorWords) {
-    minorWordsArray = minorWords.split(' ');
+    minorWordsArray = minorWords.toLowerCase().split(' ');
   }
   for (let i = 0; i < wordsArray.length; i++) {
     if (title.length > 0 && title !== '') {
       wordsArray[i] = wordsArray[i].toLowerCase();
       firstLetters[i] = wordsArray[i][0].toUpperCase();
       if (wordsArray.length > 0) {
+        console.log(wordsArray[i]);
+        console.log(minorWordsArray);
         if (i > 0 && minorWordsArray.includes(wordsArray[i])) {
           formattedWords += wordsArray[i] + ' ';
         } else {
@@ -25,9 +27,9 @@ function titleCase(title, minorWords) {
 }
 
 // console.log(titleCase(''), '');
-console.log(titleCase('a clash of KINGS', 'a an the of'), 'A Clash of Kings');
-// console.log(
-//   titleCase('THE WIND IN THE WILLOWS', 'The In'),
-//   'The Wind in the Willows'
-// );
+// console.log(titleCase('a clash of KINGS', 'a an the of'), 'A Clash of Kings');
+console.log(
+  titleCase('THE WIND IN THE WILLOWS', 'The In'),
+  'The Wind in the Willows'
+);
 // console.log(titleCase('the quick brown fox'), 'The Quick Brown Fox');
