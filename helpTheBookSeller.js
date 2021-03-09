7e436d08551ebc652bddd7095247d977f52a4c74
cambrown101 <cameron-brown@live.com>
2021-03-08 19:01:25 -0800

    Testing

--- helpTheBookSeller.js
+++ helpTheBookSeller.js
@@ -1,18 +1,22 @@
 function stockList(listOfArt, listOfCat) {
-  let res = {};
+  let ans = {};
   for (codes of listOfArt) {
     let firstLetter = codes[0];
     if (listOfCat.includes(firstLetter)) {
-      res[firstLetter] += codes.split(' ')[1];
+      if (!ans.hasOwnProperty(firstLetter)) {
+        ans[firstLetter] = parseInt(codes.split(' ')[1]);
+      } else {
+        ans[firstLetter] += parseInt(codes.split(' ')[1]);
+      }
     }
-    // console.log(res);
   }
+  return ans;
 }
 
 b = ['ABAR 200', 'CDXE 500', 'BKWR 250', 'BTSQ 890', 'DRTY 600'];
 c = ['A', 'B'];
 res = '(A : 200) - (B : 1140)';
-console.log((stockList(b, c), res));
+console.log(stockList(b, c));
 
 // b = ['CBART 20', 'CDXEF 50', 'BKWRK 25', 'BTSQZ 89', 'DRTYM 60'];
 // c = ['A', 'B', 'C', 'W'];
