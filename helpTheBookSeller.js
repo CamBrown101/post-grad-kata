f71ec059ca91e86008af51ae2926a9adb6d91020
cambrown101 <cameron-brown@live.com>
2021-03-08 20:33:35 -0800

    Clean up console.logs

--- helpTheBookSeller.js
+++ helpTheBookSeller.js
@@ -21,13 +21,3 @@ function stockList(listOfArt, listOfCat) {
   }
   return formattedAns.slice(0, -3);
 }
-
-// b = ['ABAR 200', 'CDXE 500', 'BKWR 250', 'BTSQ 890', 'DRTY 600'];
-// c = ['A', 'B'];
-// res = '(A : 200) - (B : 1140)';
-// console.log(stockList(b, c));
-
-b = ['CBART 20', 'CDXEF 50', 'BKWRK 25', 'BTSQZ 89', 'DRTYM 60'];
-c = ['A', 'B', 'C', 'W'];
-res = '(A : 0) - (B : 114) - (C : 70) - (W : 0)';
-console.log(stockList(b, c));
