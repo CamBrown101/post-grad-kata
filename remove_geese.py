a5bf66e2f2aef52baa8e2e6938cf428aced831db
cambrown101 <cameron-brown@live.com>
2021-04-07 20:26:57 -0700

    complete

--- remove_geese.py
+++ remove_geese.py
@@ -3,10 +3,8 @@ geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
 
 def goose_filter(birds):
     ans = []
+    global geese
     for bird in birds:
-        if bird in geese:
+        if not bird in geese:
             ans.append(bird)
     return ans
-
-
-print(goose_filter(["African", "Roman Tufted"]))
