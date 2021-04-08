54cc9682c0ab2af23f06c16d4d9b497d46184601
cambrown101 <cameron-brown@live.com>
2021-04-07 20:12:24 -0700

    attempt 2

--- price_of_mangoes.py
+++ price_of_mangoes.py
@@ -1,4 +1,4 @@
 def mango(quantity, price):
     threes = quantity / 3
-    ans = (threes + (quantity % 3)) * price
-    return quantity * 2
+    ans = (threes * 2 * price) + (quantity % 3)
+    return ans
